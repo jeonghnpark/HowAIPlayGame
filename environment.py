eb3111a7e831acf69d09e65337ccbdc681f117c8
jeonghnpark <jeonghn.park@gmail.com>
2021-04-06 01:55:19 +0900

    Agent

--- environment.py
+++ environment.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class Environment:
     cliff = -3
     road = -1
@@ -8,10 +9,10 @@ class Environment:
     goal_position = [2, 2]
 
     reward_list = [[road, road, road], [road, road, road], [road, road, goal]]
-    reward_list1=[["road", "road", "road"],["road","road","road"],["road","road","goal"]]
+    reward_list1 = [["road", "road", "road"], ["road", "road", "road"], ["road", "road", "goal"]]
 
     def __init__(self):
-        self.reward=np.array(self.reward_list)
+        self.reward = np.array(self.reward_list)
 
     def move(self, agent, action):
         '''
@@ -19,5 +20,25 @@ class Environment:
             agent.action=np.array([[-1,0],[0,1],[1,0],[0,-1]])
             위, 오른쪽, 아래, 왼쪽
         '''
-        new_pos=agent.pos+agent.action[action]
+        done = False
+        reward = 1
 
+        new_pos = agent.pos + agent.action[action]
+
+        observation = 1  # 움직인 후 위치, 그리드 밖이거나 goal이면 안움직임.
+        return observation, reward, done
+
+
+class Agent:
+    action = np.array([[-1, 0], [0, 1], [1, 0], [0, -1]])
+    select_action_pr=np.array([0.25, 0.25, 0.25, 0.25])
+
+    def __init__(self, initial_position):
+        self.pos=initial_position
+
+    def set_pos(self, position):
+        self.pos=position
+        return self.pos
+
+    def get_pos(self):
+        return self.pos
